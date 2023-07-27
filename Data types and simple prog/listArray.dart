faf91121b3d8cbf19f5a4bb75c5370e3647c00c4
harikris001 <hk5983835@gmail.com>
2023-07-27 18:58:20 +0530

    list creation in dart

---
+++ Data types and simple prog/listArray.dart
@@ -0,0 +1,12 @@
+List<int> numberList = [12,13,14,15,16];
+var array = [12,'name',200.0];
+
+List<String> Stringlist = ['abc','def','ghi']; // creates a list
+
+Set<int> floatList = {1,1,2,3,4,5,6,7,0,8,3}; // No repeat values in sets
+
+Map<String,String> newMap = {
+  "name" : "harikrishna",
+  "age" : "21",
+  "mail" : "abc@mail.com"
+};   // Similar to a dictionary
